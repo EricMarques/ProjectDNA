b5e10339da60710d1c172f33b6de1ccb36770c6f
EricMarques <erc_m@hotmail.com>
2019-03-14 01:14:46 -0300

    Atualizações

---
+++ codonTable.py
@@ -0,0 +1,23 @@
+codonTable = {'UUU': 'Fenilalanina',  'UUC': 'Fenilalanina',  'UUA': 'Leucina',       'UUG': 'Leucina',
+              'CUU': 'Leucina',       'CUC': 'Leucina',       'CUA': 'Leucina',       'CUG': 'Leucina',
+              'AUU': 'Isoleucina',    'AUC': 'Isoleucina',    'AUA': 'Isoleucina',    'AUG': 'Metionina',
+              'GUU': 'Valina',        'GUC': 'Valina',        'GUA': 'Valina',        'GUG': 'Valina',
+
+              'UCU': 'Serina',        'UCC': 'Serina',        'UCA': 'Serina',        'UCG': 'Serina',
+              'CCU': 'Prolina',       'CCC': 'Prolina',       'CCA': 'Prolina',       'CCG': 'Prolina',
+              'ACU': 'Treolina',      'ACC': 'Treolina',      'ACA': 'Treolina',      'ACG': 'Treolina',
+              'GCU': 'Alanina',       'GCC': 'Alanina',       'GCA': 'Alanina',       'GCG': 'Alanina',
+
+              'UAU': 'Tirosina',      'UAC': 'Tirosina',      'UAA': 'Stop',          'UAG': 'Stop',
+              'CAU': 'Histidina',     'CAC': 'Histidina',     'CAA': 'Glutamina',     'CAG': 'Glutamina',
+              'AAU': 'Asparagina',    'AAC': 'Asparagina',    'AAA': 'Lisina',        'AAG': 'Ligina',
+              'GAU': 'Ác. Aspártico', 'GAC': 'Ác. Aspártico', 'GAA': 'Ác. Glutâmico', 'GAG': 'Ác. Glutâmico',
+
+              'UGU': 'Cisteína',      'UGC': 'Cisteína',      'UGA': 'Stop',          'UGG': 'Triptofano',
+              'CGU': 'Arginina',      'CGC': 'Arginina',      'CGA': 'Arginina',      'CGG': 'Arginina',
+              'AGU': 'Serina',        'AGC': 'Serina',        'AGA': 'Arginina',      'AGG': 'Arginina',
+              'GGU': 'Glicina',       'GGC': 'Glicina',       'GGA': 'Glicina',       'GGG': 'Glicina', }
+
+startCodon = {'AUG': 'Start Codon'}
+
+stopCodon = {'UAA': 'Stop Codon', 'UAG': 'Stop Codon', 'UGA': 'Stop Codon'}
--- slice.py
+++ slice.py
@@ -1,22 +1,13 @@
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+from Bio import SeqIO
 
-'''
-#text = open('cp_teste.txt', 'r', encoding='UTF8')
-#text.close()
-#seq = 'GATCGATGGGCCTATATAGGATCGAAAATCGC'#text.read()
-
-seq = Seq('ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG', IUPAC.unambiguous_dna)
-print('DNA:  ' + seq)
-
-#seq1 = seq.transcribe()
-seq1 = seq.reverse_complement().transcribe()
-
-print('mRNA: ' + seq1)'''
+for seq_record in SeqIO.parse('NC_000019.10.fasta', 'fasta'):
+    seqDna = seq_record.seq
 
 
 def slicesequence(x):
-    print(x)
+    #print(x)
     aux = x[0:3]
     final = x[3:]
     print('\n' + aux)
@@ -26,10 +17,39 @@ def slicesequence(x):
             final = final[3:]
             print(aux)
 
-coding_dna = Seq('ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG', IUPAC.unambiguous_dna)  # Sequência DNA
-print('Seq. Original: DNA = {}' .format(coding_dna))
-coding_rna = coding_dna.transcribe()  # Trenscrever DNA para RNAm
-print('Seq. Original: RNA = {}' .format(coding_rna))
-print('Caracteres Seq. Orig: {}'.format(len(coding_dna)))
+    return 0
+    '''
+    print('1')
+    aux = x[0:3]
+    print(aux)
+    final = x[3:]
+    print(final)
+    print('2')
+    aux = final[:3]
+    print(aux)
+    final = final[3:]
+    print(final)
+    ...
+    '''
 
-print(slicesequence(coding_rna))  # Chama a função slicesequence() passando como parâmetro a sequência RNAm
+'''
+#text = open('cp_teste.txt', 'r', encoding='UTF8')
+#text.close()
+'''
+'''
+coding_dna = Seq('ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG', IUPAC.unambiguous_dna)  # Sequência DNA
+print('Seq. Original: DNA =  {}' .format(coding_dna))
+messenger_rna = coding_dna.transcribe()  # Trenscrever DNA para RNAm
+print('Seq. Original: RNAm = {}' .format(messenger_rna))
+'''
+
+seqDNA = str(seqDna)
+codeSeqDNA = Seq(seqDNA, IUPAC.unambiguous_dna)
+messengerRNA = codeSeqDNA.transcribe()
+
+print('DNA: {}\n'.format(seqDna))
+print('Caracteres DNA: {}'.format(len(seqDNA)))
+print('\nRNAm: {}\n' .format(messengerRNA))
+print('Caracteres RNAm: {}'.format(len(messengerRNA)))
+
+print(slicesequence(seqDNA))  # Chama a função slicesequence() passando como parâmetro a sequência RNAm
