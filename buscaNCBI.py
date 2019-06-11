3a5b15277184a5022203c4fcb343761e7ba0c67d
Eric Marques <erc_m@hotmail.com>
2019-06-11 15:42:34 -0300

    alteração buscaNCBI.py

--- buscaNCBI.py
+++ buscaNCBI.py
@@ -2,13 +2,36 @@ from Bio.Blast import NCBIWWW
 
 path = "Samples/"
 
-file_request = input("Digite o nome da Proteína a ser buscada: ")
+query = input("Digite o nome da Proteína a ser buscada: ")
+
+'''
+arq_file = path + query
+
+if os.path.isfile(arq_file):
+    try:
+        print("Arquivo '" + query + "' encontrado!")
+        arq = open(path + query, "r")
+        if query not in arq_file:
+            raise NameError(path + query)
+
+    except NameError:
+        print("OPA")
+        raise
+
+    finally:
+
+        arq = open(path + query, "r")
+        print(arq.read())
+
+print(os.path.isfile(arq_file))
+print(arq_file)
+'''
 
 # Buscar proteínas = blastp
 # Buscar nucleotídeos = blastn
 print("Buscando...")
-blast_result = NCBIWWW.qblast("blastp", "nr", file_request)
-blast_out = open(path + file_request + ".xml", "w")
+blast_result = NCBIWWW.qblast("blastp", "nr", query)
+blast_out = open(path + query + ".xml", "w")
 blast_out.write(blast_result.read())
 blast_out.close()
 blast_result.close()
